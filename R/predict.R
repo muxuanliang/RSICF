7f696fa5c46aadd7b280777ef9480111674c2471
Muxuan Liang <10004752+muxuanliang@users.noreply.github.com>
2019-12-19 15:05:02 -0800

    debug

--- R/predict.R
+++ R/predict.R
@@ -3,7 +3,7 @@ predict.rsfit <- function(fit, newx, lossType = 'logistic', type = 'transformed'
   splitNumber <- length(fit)
   res_all <- sapply(fit, function(t){
     z = newx %*% t$beta
-    pre <- cbind(1,splines::bs(z, knots=t$knots[2:(length(t$knots)-1)], intercept = FALSE, Boundary.knots = c(t$knots[1], t$knots[length(t$knots)])))%*%t$xi
+    pre <- splines::ns(z, knots=t$knots[2:(length(t$knots)-1)], intercept = TRUE, Boundary.knots = c(t$knots[1], t$knots[length(t$knots)]))%*%t$xi
     if(sum(abs(t$xi))<1e-5){
       pre <- z
     }
@@ -16,21 +16,18 @@ predict.rsfit <- function(fit, newx, lossType = 'logistic', type = 'transformed'
 }
 
 # predict.rsfit gets the predicted treatment for a splitted fit
-predict.rsfitSplit <- function(fit, newx, derivative = FALSE,lossType = 'logistic', type = 'value'){
+predict.rsfitSplit <- function(fit, newx, derivative = FALSE){
   t <- fit
   z <- newx %*% t$beta
-  pre <- cbind(1,splines::bs(z, knots=t$knots[2:(length(t$knots)-1)], intercept = FALSE, Boundary.knots = c(t$knots[1], t$knots[length(t$knots)])))%*%t$xi
+  pre <- splines::ns(z, knots=t$knots[2:(length(t$knots)-1)], intercept = TRUE, Boundary.knots = c(t$knots[1], t$knots[length(t$knots)]))%*%t$xi
   if(sum(abs(t$xi))<1e-5){
     pre <- z
   }
   if(derivative){
-    pre <- cbind(0, nsd(z, knots=t$knots[2:(length(t$knots)-1)], intercept = FALSE, Boundary.knots = c(t$knots[1], t$knots[length(t$knots)])))%*%t$xi
+    pre <- nsd(z, knots=t$knots[2:(length(t$knots)-1)], intercept = TRUE, Boundary.knots = c(t$knots[1], t$knots[length(t$knots)]))%*%t$xi
     if(sum(abs(t$xi))<1e-5){
       pre <- 1
     }
-  }
-  if (type=='eta'){
-    pre <- link(pre, lossType = lossType)
   }
   pre
 }
